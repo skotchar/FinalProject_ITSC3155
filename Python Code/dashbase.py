97364c1a80e9c2231a994515e6f0ea754df1fdb6
Paul O'Meara <pomeara@uncc.edu>
2021-04-20 16:20:13 -0400

    -added a python code section

---
+++ Python Code/dashbase.py
@@ -0,0 +1,9 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+#TODO create basis of html and complete css before thursday
+import dash
+
+
