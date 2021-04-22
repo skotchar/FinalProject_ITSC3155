12ae5969ef0b690b16d3756cc4c0fe0331b247d6
Paul O'Meara <pomeara@uncc.edu>
2021-04-22 15:54:01 -0400

    -added html to reflect changes do not remove Python Code directory

--- Python Code/dashbase.py
+++ Python Code/dashbase.py
@@ -1,8 +1,3 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
 import dash
 import dash_html_components as html
 import dash.dependencies as dd
@@ -18,7 +13,7 @@ emissions_css = ['css/emission_planner.css']
 logo = ['/localhost/images/Emissions Planner Logo.jpg', '/localhost/images/HelpButton.jpg']
 # TODO Figure out why image is not showing up, it might be a bug with dash?
 # TODO create choropleth map as fig
-df = pd.read_csv('datasets/API_EN.ATM.CO2E.KT_DS2_en_csv_v2_2163797.csv')
+df = pd.read_csv('../Html Code/datasets/API_EN.ATM.CO2E.KT_DS2_en_csv_v2_2163797.csv')
 
 fig = go.Figure(data=go.Choropleth(
     locations = df['Country Name'],
@@ -69,7 +64,7 @@ def home_layout(logos, figures):
                     html.H2('Select a country from map')]),
                 html.Div(id='home_bottom_right', children=[
                     html.Div(id='home_map_back', children=[dcc.Graph(id='choropleth', figure=figures)])
-                ])
+                ]),
             ])
         ])
 
@@ -85,7 +80,7 @@ def home_layout(logos, figures):
 
 
 def visualization_layout(logos, figures, dissable_button=False):
-    return 1
+    return html.Button('?', id='visualization_tutorial_button', n_clicks=0)
 
 
 """
@@ -104,7 +99,7 @@ def visual_tutorial_layout(logos, arrows, figures, dissable_non_tutorial_button=
             html.Button('Quit', id='stop_tutorial', n_clicks=0),
             html.Div(id='tutorial_back_panel_left', hidden=False, children=[
                 html.Button('Previous', id='previous_button_left', n_clicks=0, disabled=False, hidden=False),
-                html.Progress(id='tutorial_progress_left', value=1, max=10, hidden=False, children=[
+                html.Progress(id='tutorial_progress_left', value='1', max=10, hidden=False, children=[
                     html.P('1/10', id='counter_left', hidden=False)]),
                 html.Button('Next', id='next_button_left', n_clicks=0, disabled=False, hidden=False)
             ]),
@@ -116,7 +111,7 @@ def visual_tutorial_layout(logos, arrows, figures, dissable_non_tutorial_button=
             ]),
             html.Div(id='tutorial_back_panel_right', hidden=True, children=[
                 html.Button('Previous', id='previous_button_right', n_clicks=0, disabled=True, hidden=True),
-                html.Progress(id='tutorial_progress_right', value=1, max=10, hidden=True, children=[
+                html.Progress(id='tutorial_progress_right', value='1', max=10, hidden=True, children=[
                     html.P('1/10', id='counter_right', hidden=True)]),
                 html.Button('Next', id='next_button_right', n_clicks=0, disabled=True, hidden=True)
             ]),
@@ -138,21 +133,33 @@ def switch_layout(val, logos, arrows, figures, dissable_button):
     Callbacks for button elements
 """
 
-
 @app.callback(
     dd.Output('page', 'children'),
-    [dd.Input('home_tutorial_button' or 'visualization_tutorial_button', 'n_clicks')])
+    dd.Input('home_tutorial_button', 'n_clicks'),
+    dd.Input('visualization_tutorial_button', 'n_clicks'),
+    dd.Input('stop_tutorial', 'n_clicks'), suppress_callback_exceptions=True)
 def switch_to_tutorial_mode(value):
     # TODO switch home page to visualization and start tutorial mode
+    ctx = dash.callback_context
+    input_id = ctx.triggered[0]["prop_id"].split(".")[0]
+    print(input_id)
     if value == 0:
         raise PreventUpdate
-    else:
+    elif input_id == 'stop_tutorial':
+        logos = logo
+        arrows = arrow
+        figures = fig
+        dissable_button = False
+        value = 0
+        return switch_layout(2, logos, arrows, figures, dissable_button)
+    elif input_id == 'home_tutorial_button' or input_id == 'visualization_tutorial_button':
         logos = logo
         arrows = arrow
         figures = fig
         dissable_button = True
         value = 0
         return switch_layout(1, logos, arrows, figures, dissable_button)
+
 
 if __name__ == '__main__':
     # start up on the home page
