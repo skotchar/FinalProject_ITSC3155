b9ce7a4fa2a17867202d29d4a953008b9ec5ee0d
Paul O'Meara <pomeara@uncc.edu>
2021-04-29 16:33:16 -0400

    -change home layout and added general javascript

---
+++ Html Code/scripts/emissions.js
@@ -0,0 +1,18 @@
+function generateCMap() {
+    /*
+        put choropleth map here
+    */
+}
+
+function setupSelectorsOptions() {
+    /*
+        TODO do code to read csv and add countries to dropdown list
+        first scan page for <select> tag
+        second read csv
+        third add options to list
+    */
+}
+
+function selectACountry(id)" {
+    return id.value
+}
