bdc0af791d5fed0260955c817266f6492c43036a
Paul O'Meara <pomeara@uncc.edu>
2021-04-30 15:49:21 -0400

    -select a country dropdown is not working im not sure if any of you can work on setupSelectorsOptions() function in emissions.js

--- Html Code/scripts/emissions.js
+++ Html Code/scripts/emissions.js
@@ -1,10 +1,19 @@
-function generateCMap() {
-    /*
-        put choropleth map here
-    */
-}
+const csv_data = fetch("./datasets/worldMapData (2).json")
+.then(response => {
+   return response.json();
+})
+.then(data => console.log(data));
 
-function setupSelectorsOptions() {
+function setupSelectorsOptions(element) {
+    console.log(csv_data)
+    for (x = 0; x < csv_data.values.length; x++ ) {
+        single_entry = csv_data.values[x].name;
+        console.log(single_entry)
+        newElement = document.createElement("option")
+        newElement.value = single_entry
+        newElement.innerHTML = single_entry
+        element.appendChild(newElement)
+    }
     /*
         TODO do code to read csv and add countries to dropdown list
         first scan page for <select> tag
